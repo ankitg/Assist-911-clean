a1d82c5159aa5af2c9d272db6848b0af34776f69
Ankit Guglani <ankitg@gmail.com>
2015-05-04 00:51:56 -0400

    • Added voice recognition • Added audio playback • Refactored TTS and Media player out of the application (left in as comments for reference) • Increased min SDK to API version 15

--- app/src/main/java/com/stmichaelshospital/assist911/Assist911Application.java
+++ app/src/main/java/com/stmichaelshospital/assist911/Assist911Application.java
@@ -2,23 +2,23 @@ package com.stmichaelshospital.assist911;
 
 import android.app.Application;
 import android.content.SharedPreferences;
-import android.os.Build;
-import android.speech.tts.TextToSpeech;
-import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Locale;
 
 /**
  * Created by ankitg on 2015-04-28.
  */
-public class Assist911Application extends Application implements TextToSpeech.OnInitListener{
+public class Assist911Application extends Application // implements TextToSpeech.OnInitListener
+{
 
     public static SharedPreferences _SharedPreferences = null;
     public static SharedPreferences.Editor _SharedPreferencesEditor = null;
     public static ArrayList<VideoItem> _VideosArray = new ArrayList<VideoItem>();
-    private static TextToSpeech _TextToSpeech;
-    public static Boolean isTTSAvailable = false;
+
+//    private static TextToSpeech _TextToSpeech;
+//    public static Boolean isTTSAvailable = false;
+
+//    private static MediaPlayer _MediaPlayer;
 
     public static final Boolean isDevMode = true;
 
@@ -27,7 +27,9 @@ public class Assist911Application extends Application implements TextToSpeech.On
         super.onCreate();
         populateVideosArray();
         configureSharedPreferences();
-        _TextToSpeech = new TextToSpeech(this, this);
+
+//        _TextToSpeech = new TextToSpeech(this, this);
+//        _MediaPlayer = new MediaPlayer();
     }
 
     private void configureSharedPreferences() {
@@ -35,7 +37,7 @@ public class Assist911Application extends Application implements TextToSpeech.On
         _SharedPreferencesEditor = _SharedPreferences.edit();
     }
 
-    private static void populateVideosArray() {
+    private void populateVideosArray() {
         _VideosArray.add(new VideoItem("Flames", "flame", VideoItem.EMERGENCYTYPE.FIRE));
         _VideosArray.add(new VideoItem("Smoke", "smoke", VideoItem.EMERGENCYTYPE.FIRE));
         _VideosArray.add(new VideoItem("Car Thief", "car", VideoItem.EMERGENCYTYPE.POLICE));
@@ -45,29 +47,59 @@ public class Assist911Application extends Application implements TextToSpeech.On
         _VideosArray.add(new VideoItem("Family Playing Soccer", "b", VideoItem.EMERGENCYTYPE.NONE));
     }
 
+//    @Override
+//    public void onInit(int status) {
+//        if (status == TextToSpeech.SUCCESS) {
+//            int result = _TextToSpeech.setLanguage(Locale.US);
+//            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+//                // Language is not supported. Install the language.
+//                Toast.makeText(this, "Please install US English TTS pack for verbal prompts", Toast.LENGTH_LONG).show();
+//            } else {
+//                isTTSAvailable = true;
+//            }
+//        } else {
+//            isTTSAvailable = false;
+//        }
+//    }
+//
+//    public static void say(String text) {
+//        if(isTTSAvailable) {
+//            if (Integer.parseInt(Build.VERSION.SDK) < 21) {
+//                _TextToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+//            } else {
+//                _TextToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+//            }
+//        }
+//    }
+
+//    public static void say(String text, Context context, int resource, MediaPlayer.OnCompletionListener onCompletedPlaying) {
+//        if(isTTSAvailable) {
+//            say(text);
+//        } else {
+//            playAudio(context, resource, onCompletedPlaying);
+//        }
+//    }
+//
+//    public static void playAudio (Context context, int resource, MediaPlayer.OnCompletionListener onCompletedPlaying) {
+//        _MediaPlayer = MediaPlayer.create(context, resource);
+//        _MediaPlayer.setOnCompletionListener(onCompletedPlaying);
+//        _MediaPlayer.start();
+//    }
+
     @Override
-    public void onInit(int status) {
-        if (status == TextToSpeech.SUCCESS) {
-            int result = _TextToSpeech.setLanguage(Locale.US);
-            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                // Language is not supported. Install the language.
-                Toast.makeText(this, "Please install US English TTS pack for verbal prompts", Toast.LENGTH_LONG).show();
-            } else {
-                isTTSAvailable = true;
-            }
-        } else {
-            // TODO: fallback to recorded audio
-        }
-    }
+    public void onTerminate() {
+        super.onTerminate();
 
-    public static void say(String text) {
-        if(isTTSAvailable) {
-            if (Integer.parseInt(Build.VERSION.SDK) < 21) {
-                _TextToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
-            } else {
-                _TextToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
-            }
-        }
-    }
+        _VideosArray = null;
 
+//        if (_TextToSpeech != null) {
+//            _TextToSpeech.stop();
+//            _TextToSpeech.shutdown();
+//        }
+
+//        if (_MediaPlayer != null) {
+//            _MediaPlayer.stop();
+//            _MediaPlayer = null;
+//        }
+    }
 }
